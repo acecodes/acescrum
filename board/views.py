defd7d75d0654232754439f4a6c035cdf169052b
Ace <ace.github@gmail.com>
2014-12-16 18:05:47 -0800

    Added first pieces of the REST API

--- board/views.py
+++ board/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import authentication, permissions, viewsets
+from .models import Sprint
+from .serializers import SprintSerializer
 
-# Create your views here.
+class DefaultsMixin(object):
+	"""Default settings for view authentication, permissions, filtering
+	and pagination."""
+
+	authentication_classes = (
+		authentication.BasicAuthentication,
+		authentication.TokenAuthentication,
+		)
+	permission_classes = (
+		permissions.IsAuthenticated,
+		)
+	paginate_by = 25
+	paginate_by_param = 'page_size'
+	max_paginate_by = 100
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
+	"""API Endpoint for listing and creating sprints."""
+
+	queryset = Sprint.objects.order_by('end')
+	serializer_class = SprintSerializer
