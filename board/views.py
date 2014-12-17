a440dc5b04bfe1ab03ce1a8e3c879986e9b64aaa
Ace <ace.github@gmail.com>
2014-12-16 18:19:58 -0800

    Functional API endpoints

--- board/views.py
+++ board/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from rest_framework import authentication, permissions, viewsets
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+User = get_user_model()
 
 class DefaultsMixin(object):
 	"""Default settings for view authentication, permissions, filtering
@@ -18,7 +21,19 @@ class DefaultsMixin(object):
 	max_paginate_by = 100
 
 class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
-	"""API Endpoint for listing and creating sprints."""
+	"""API endpoint for listing and creating sprints."""
 
 	queryset = Sprint.objects.order_by('end')
 	serializer_class = SprintSerializer
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+	"""API endpoint for listing and creating tasks."""
+	queryset = Task.objects.all()
+	serializer_class = TaskSerializer
+
+class UserViewSet(DefaultsMixin, viewsets.ModelViewSet):
+	"""API endpoint for listing users."""
+	lookup_field = User.USERNAME_FIELD
+	lookup_url_kwarg = User.USERNAME_FIELD
+	queryset = User.objects.order_by(User.USERNAME_FIELD)
+	serializer_class = UserSerializer
