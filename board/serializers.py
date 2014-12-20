63a2696c5cc636b941868c33b72b9d5b7c933588
Ace <ace.github@gmail.com>
2014-12-20 12:36:13 -0800

    Cleaned up errors and added filters

--- board/serializers.py
+++ board/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework import reverse
+from rest_framework.reverse import reverse
 from django.contrib.auth import get_user_model
 from .models import Sprint, Task
 
@@ -25,11 +25,11 @@ class SprintSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
 	full_name = serializers.CharField(source='get_full_name', read_only=True)
-	links = serializers.SerializerMethodField('get_links')
+	links = serializers.SerializerMethodField()
 
 	class Meta:
 		model = User
-		fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
+		fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', 'links',)
 
 	def get_links(self, obj):
 		request = self.context['request']
@@ -37,7 +37,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 		return {
 		'self': reverse('user-detail',
-			kwargs={User.USERNAME_FILED:username}, request=request),
+			kwargs={User.USERNAME_FIELD:username}, request=request),
 		}
 
 class SprintSerializer(serializers.ModelSerializer):
@@ -49,8 +49,8 @@ class SprintSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
 
 	assigned = serializers.SlugRelatedField(slug_field=User.USERNAME_FIELD, required=False, queryset=User.objects.all())
-	status_display = serializers.SerializerMethodField('get_status_display')
-	links = serializers.SerializerMethodField('get_links')
+	status_display = serializers.SerializerMethodField()
+	links = serializers.SerializerMethodField()
 
 	class Meta:
 		model = Task
@@ -62,8 +62,7 @@ class TaskSerializer(serializers.ModelSerializer):
 
 	def get_links(self, obj):
 		request = self.context['request']
-		links = {'self':reverse('task-detail',
-			kwargs={'pk':obj.pk}, request=request),
+		links = {'self':reverse('task-detail', kwargs={'pk':obj.pk}, request=request),
 		'sprint':None,
 		'assigned':None
 		}
