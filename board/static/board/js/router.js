82dea0f5217383ee461feb98c9f5ec27afd4cb7f
Ace <ace.github@gmail.com>
2015-01-02 21:25:39 -0800

    Working on views, still not functional

--- board/static/board/js/router.js
+++ board/static/board/js/router.js
@@ -1,12 +1,16 @@
 (function ($, Backbone, _, app) {
 	var AppRouter = Backbone.Router.extend({
-		routes: {
-			'':'home'
-		},
+			
+			routes: {
+				'':'home'
+			},
 		
 			initialize: function (options) {
 				this.contentElement = '#content';
 				this.current = null;
+				this.header = new app.views.HeaderView();
+				$('body').prepend(this.header.el);
+				this.header.render();
 				Backbone.history.start();
 
 			},
@@ -14,6 +18,30 @@
 			home: function () {
 				var view = new app.views.HomepageView({el: this.contentElement});
 				this.render(view);
+			},
+
+			route: function (route, name, callback) {
+				var login;
+				callback = callback || this[name];
+				callback = _.wrap(callback, function (original) {
+					var args = _.without(arguments, original);
+					if (app.session.authenticated()) {
+						original.apply(this, args);
+					} else {
+						// Show login screen before calling the view
+						$(this.contentElement).hide()
+						login = new app.views.LoginView();
+						$(this.contentElement).after(login.el);
+						login.on('done', function () {
+							this.header.render();
+							$(this.contentElement).show();
+							original.apply(this, args);
+						}, this);
+						login.render();
+					}
+				});
+
+				return Backbone.Router.prototype.route.apply(this, [route, name, callback]);
 			},
 
 			render: function (view) {
@@ -24,8 +52,6 @@
 				this.current = view;
 				this.current.render();
 			}
-
-
 	});
 
 	app.router = AppRouter;
