b7db820093a8e43f83f61f1725968ca396c3ff0f
Ace <ace.github@gmail.com>
2015-01-20 15:25:49 -0800

    Changed settings arrangement, added Heroku

--- acescrum/wsgi.py
+++ acescrum/wsgi.py
@@ -6,9 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acescrum.settings")
-
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acescrum.settings")
+application = Cling(get_wsgi_application())
