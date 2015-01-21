91ed2ba6c2ae9355bbc97e550d83a4aeca25a67c
Ace <ace.github@gmail.com>
2015-01-20 16:22:39 -0800

    Static files fix

--- acescrum/urls.py
+++ acescrum/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
     url(r'^$', TemplateView.as_view(template_name='board/index.html')),
+    urlpatterns += patterns('',
+                            (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                             {'document_root': settings.STATIC_ROOT}),
+                            )
 ]
