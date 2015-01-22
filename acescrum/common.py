b250059a8973eda5ba252a6276ff46a0b5701a76
Ace <ace.github@gmail.com>
2015-01-22 12:27:20 -0800

    Added admin feature

--- acescrum/common.py
+++ acescrum/common.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- acescrum/urls.py
+++ acescrum/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import include, url, patterns
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 from board.urls import router
+from django.contrib import admin
 import acescrum.common
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
     url(r'^$', TemplateView.as_view(template_name='board/index.html')),
