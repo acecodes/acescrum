5b19054e99b83ae4e7ebb7e206bcb2ecb1a606ae
Ace <ace.github@gmail.com>
2015-01-26 09:06:26 -0800

    Added Tornado options and graceful shutdown

--- tornado_server.py
+++ tornado_server.py
@@ -1,7 +1,19 @@
 from urllib.parse import urlparse
 from tornado.ioloop import IOLoop
 from tornado.web import Application
+from tornado.options import define, parse_command_line, options
 from tornado.websocket import WebSocketHandler
+from tornado.httpserver import HTTPServer
+
+import logging
+import signal
+import time
+
+
+define('debug', default=False, type=bool, help='Run in debug mode')
+define('port', default=8080, type=int, help='Server port')
+define('allowed_hosts', default='localhost:8080', multiple=True,
+       help='Allowed hosts for cross-domain connections')
 
 
 class SprintHandler(WebSocketHandler):
@@ -10,7 +22,8 @@ class SprintHandler(WebSocketHandler):
     def check_origin(self, origin):
         allowed = super().check_origin(origin)
         parsed = urlparse(origin.lower())
-        return allowed or parsed.netloc.startswith('localhost:')
+        matched = any(parsed.netloc == host for host in options.allowed_hosts)
+        return options.debug or allowed or matched
 
     def open(self, sprint):
         """Subscribe to sprint updates on a new connection"""
@@ -21,8 +34,24 @@ class SprintHandler(WebSocketHandler):
     def on_close(self):
         """Remove subscription"""
 
+
+def shutdown(server):
+    ioloop = IOLoop.instance()
+    logging.info('Stopping server')
+    server.stop()
+
+    def finalize():
+        ioloop.stop()
+        logging.info('Stopped')
+
+    ioloop.add_timeout(time.time() + 1.5, finalize)
+
 if __name__ == '__main__':
+    parse_command_line()
     application = Application([
         (r'/(?P<sprint>[0-9]+)', SprintHandler)])
-    application.listen(8080)
+    server = HTTPServer(application)
+    server.listen(options.port)
+    signal.signal(signal.SIGINT, lambda sig, frame: shutdown(server))
+    logging.info('Starting server on localhost:{0}'.format(options.port))
     IOLoop.instance().start()
