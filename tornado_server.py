4903fd7aaec6d94b0659f0c3c4a057ebd2138b07
Ace <ace.github@gmail.com>
2015-02-06 17:08:49 -0800

    First steps towards implementing Redis

--- tornado_server.py
+++ tornado_server.py
@@ -5,6 +5,9 @@ from tornado.options import define, parse_command_line, options
 from tornado.websocket import WebSocketHandler, WebSocketClosedError
 from tornado.httpserver import HTTPServer
 from collections import defaultdict
+from redis import Redis
+from tornadoredis import Client
+from tornadoredis.pubsub import BaseSubScriber
 
 import logging
 import signal
@@ -62,6 +65,21 @@ class SprintHandler(WebSocketHandler):
         """Remove subscription"""
         self.application.remove_subscriber(self.sprint, self)
 
+class RedisSubscriber(BaseSubScriber):
+
+    def on_message(self, msg):
+        """Handle new message on Redis channel"""
+        if msg and msg.kind == 'message':
+            subscribers = list(self.subscribers[msg.channel].keys())
+            for subscriber in subscribers:
+                try:
+                    subscriber.write_message(msg.body)
+                except tornado.websocket.WebSocketClosedError:
+                    # Remove dead peer
+                    self.unsubscribe(msg.channel, subscriber)
+            super().on_message(msg)
+
+
 
 class ScrumApplication(Application):
 
@@ -71,30 +89,19 @@ class ScrumApplication(Application):
          (r'/(?P<model>task|sprint|user)/(?P<pk>[0-9]+)', UpdateHandler),
         ]
         super().__init__(routes, **kwargs)
-        self.subscriptions = defaultdict(list)
+        self.subscriber = RedisSubscriber(Client())
+        self.publisher = Redis()
 
     def add_subscriber(self, channel, subscriber):
-        self.subscriptions[channel].append(subscriber)
+        self.subscriber.subscribe(['all', channel], subscriber)
 
     def remove_subscriber(self, channel, subscriber):
-        self.subscriptions[channel].remove(subscriber)
-
-    def get_subscribers(self, channel):
-        return self.subscriptions[channel]
+        self.subscriber.unsubscribe(channel, subscriber)
+        self.subscriber.unsubscribe('all', subscriber)
 
     def broadcast(self, message, channel=None, sender=None):
-        if channel is None:
-            for c in self.subscriptions.keys():
-                self.broadcast(message, channel=c, sender=sender)
-        else:
-            peers = self.get_subscribers(channel)
-            for peer in peers:
-                if peer != sender:
-                    try:
-                        peer.write_message(message)
-                    except WebSocketClosedError:
-                        # Remove dead peer
-                        self.remove_subscriber(channel, peer)
+        channel = 'all' if channel is None else channel
+        self.publisher.publish(channel, message)
 
 
 def shutdown(server):
